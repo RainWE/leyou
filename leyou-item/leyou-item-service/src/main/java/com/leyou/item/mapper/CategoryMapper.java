202c8a6410e7e0b39c57bf020d913d8893a0ab1f
CDX <1138224907@qq.com>
2020-05-20 20:28:03 +0800

    解决跨域 品牌查询

---
+++ leyou-item/leyou-item-service/src/main/java/com/leyou/item/mapper/CategoryMapper.java
@@ -0,0 +1,14 @@
+package com.leyou.item.mapper;
+
+
+import com.leyou.item.pojo.Category;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * @Auther:cdx
+ * @Date:2020-05-20
+ * @Description:com.leyou.item.mapper
+ * @Version:1.0
+ */
+public interface CategoryMapper extends Mapper<Category> {
+}
---
+++ leyou-item/leyou-item-service/src/main/java/com/leyou/item/service/CategoryService.java
@@ -0,0 +1,33 @@
+package com.leyou.item.service;
+
+import com.leyou.item.mapper.CategoryMapper;
+import com.leyou.item.pojo.Category;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @Auther:cdx
+ * @Date:2020-05-20
+ * @Description:com.leyou.item.service
+ * @Version:1.0
+ */
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryMapper categoryMapper;
+
+    /**
+     * 根据父节点查询子节点
+     * @param pid
+     * @return
+     */
+    public List<Category> queryCategoriesByPid(Long pid) {
+        Category record = new Category();
+        record.setParentId(pid);
+        return this.categoryMapper.select(record);
+
+    }
+}
